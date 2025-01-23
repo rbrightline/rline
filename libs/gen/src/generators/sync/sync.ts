b9b603e949b34aa897463d0d325a4d5884879653
Robert Brightline <robert.brightline@gmail.com>
2025-01-22 21:39:37 -0600

    improving schemas for project requirements

--- libs/gen/src/generators/sync/sync.ts
+++ libs/gen/src/generators/sync/sync.ts
@@ -1,16 +1,6 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
-import * as path from 'path';
-import { SyncGeneratorSchema } from './schema';
+import { formatFiles, Tree } from '@nx/devkit';
 
-export async function syncGenerator(tree: Tree, options: SyncGeneratorSchema) {
-  const projectRoot = `libs/type/src/lib`;
-
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+export async function syncGenerator(tree: Tree) {
   await formatFiles(tree);
 }
 
--- libs/gen/src/index.ts
+++ libs/gen/src/index.ts
@@ -5,3 +5,4 @@ export * from './generators/dto/dto';
 export * from './generators/entity/entity';
 export * from './generators/init/init';
 export * from './generators/model/model';
+export * from './generators/sync/sync';
